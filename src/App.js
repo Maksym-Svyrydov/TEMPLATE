1bc1b66e1a592faa7ee995ccbe12e5f9d6d00414
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-05-01 15:54:24 +0300

    deploy-5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { SharedLayout } from './components/SharedLayuot';
-import { Routes, Route } from 'react-router-dom';
-import { lazy } from 'react';
+// import { SharedLayout } from './components/SharedLayuot';
+// import { Routes, Route } from 'react-router-dom';
+// import { lazy } from 'react';
 
-const Home = lazy(() => import('./pages/Home'));
+// const Home = lazy(() => import('./pages/Home'));
 // const Tweets = lazy(() => import('./pages/Tweets'));
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-        <Route path="*" />
-        {/* <Route path="tweet" element={<Tweets />} />
-        <Route path="*" /> */}
-      </Route>
-    </Routes>
+    <div>TEMPLATE</div>
+    // <Routes>
+    //   <Route path="/" element={<SharedLayout />}>
+    //     <Route index element={<Home />} />
+    //     <Route path="*" />
+    //     {/* <Route path="tweet" element={<Tweets />} />
+    //     <Route path="*" /> */}
+    //   </Route>
+    // </Routes>
   );
 };
 export default App;
